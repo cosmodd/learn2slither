8213a7ab7c81da0840ee5bfd0405f6fe84926dde
Maxim RATTEZ <lolesqua@gmail.com>
2026-06-12 15:09:21 +0200

    chore: remove unused code

--- main.py
+++ main.py
@@ -4,33 +4,6 @@ from Game import Game, Directions, GameStates, MoveOutcome
 from Renderer import Renderer
 
 
-def print_game(game: Game):
-    (width, height) = game.size
-
-    for y in range(height + 2):
-        for x in range(width + 2):
-            char = '  '
-
-            if x < 1 or y < 1 or x > width or y > height:
-                # char = '██'
-                char = '🧱'
-
-            game_pos = (x - 1, y - 1)
-
-            if game_pos in game.snake:
-                char = '🟢'
-            if game_pos == game.snake[0]:
-                char = '🤢'
-
-            if game_pos in game.green_apples:
-                char = '🍏'
-
-            if game_pos in game.red_apples:
-                char = '🍎'
-
-            print(char, end='')
-        print()
-
 def main():
     # Setup pygame
     pygame.init()
